ec7a4edb9fa18d17c50fd5a8c5206e6754f7d5dd
IM-TECHNO <raagul3105@gmail.com>
2026-08-27 12:31:17 +0530

    Add Google Analytics scripts to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Geist, Geist_Mono } from 'next/font/google';
-
+import Script from 'next/script';
 
 const geistSans = Geist({
   subsets: ['latin'],
@@ -26,6 +26,18 @@ export default function Layout({ children }: LayoutProps<'/'>) {
     >
       <body className="flex flex-col min-h-screen font-sans">
         <RootProvider>{children}</RootProvider>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-E053F2EQE8"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-E053F2EQE8');
+          `}
+        </Script>
       </body>
     </html>
   );
